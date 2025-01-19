b6294749bab5e5a8e6a547f3255a38b6a6b2e491
michelobe <michaeldlr17@gmail.com>
2025-01-19 12:04:52 -0800

    Still trying to fix netlify deploy, dependencies reinstalled, reverted next.config.mjs to default

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
-/** @type {import('next').NextConfig} */
-import withNetlify from 'next-on-netlify';
-
-const nextConfig = {
-  // Any other Next.js config options you want to include
+// next.config.js
+module.exports = {
+    reactStrictMode: true,
 };
-
-export default withNetlify(nextConfig);
